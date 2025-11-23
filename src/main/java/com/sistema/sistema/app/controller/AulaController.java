4bcac0019a6dcf9025cbf0df78432a65ad564557
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-11-23 00:06:42 -0300

    a
    a

--- src/main/java/com/sistema/sistema/app/controller/AulaController.java
+++ src/main/java/com/sistema/sistema/app/controller/AulaController.java
@@ -11,6 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,9 +29,10 @@ public class AulaController {
 
     private final Algorithm algorithm = Algorithm.HMAC256("secreto-muito-forte-ALTERE-AGORA");
 
-    @GetMapping
+    @GetMapping("/{id}")
     @PreAuthorize("isAuthenticated()")
-    public String cursoPage() {
+    public String cursoPage(@PathVariable String id, Model model) {
+        model.addAttribute("aulaId", id);
         return "aula";
     }
 
